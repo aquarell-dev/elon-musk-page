fccb360ca5ce964a3265a31daddd1a1a41aa49ad
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-11 00:58:31 +0300

    outside hook

---
+++ src/hooks/useOutside.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+
+export const useOutside = <T extends HTMLElement = HTMLElement, U = void>(
+	ref: React.RefObject<T>,
+	callback: () => U
+) => {
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (ref.current && !ref.current.contains(event.target as Node)) {
+				callback()
+			}
+		}
+
+		document.addEventListener('mousedown', handleClickOutside)
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [ref, callback])
+}
