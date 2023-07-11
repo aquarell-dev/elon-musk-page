bc9d29678d39a3514af293221e23ec3ee86df580
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-11 18:22:21 +0300

    benefits prefetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import { default as HomeScreen } from '@/components/screens/home'
-import { NextPage } from 'next'
+import { Benefit } from '@/features/benefits/benefits.types'
+import { useActions } from '@/hooks/useActions'
+import { defaultAxios } from '@/utils'
+import { GetStaticProps, NextPage } from 'next'
+import { useEffect } from 'react'
 
-const Home: NextPage = () => {
+type HomeProps = {
+	benefits: Benefit[]
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async context => {
+	const r = await defaultAxios.get<Benefit[]>('benefits/')
+
+	return {
+		props: {
+			benefits: r.data
+		},
+		revalidate: 300
+	}
+}
+
+const Home: NextPage<HomeProps> = ({ benefits }) => {
+	const { setBenefits } = useActions()
+
+	useEffect(() => {
+		setBenefits(benefits)
+	}, [])
+
 	return <HomeScreen />
 }
 
