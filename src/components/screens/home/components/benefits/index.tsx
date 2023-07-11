2484454914c04b8f6c1498b3484ab07a8bf967b3
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-11 18:22:29 +0300

    getting benefits from store

--- src/components/screens/home/components/benefits/index.tsx
+++ src/components/screens/home/components/benefits/index.tsx
@@ -1,42 +1,9 @@
-import { Benefit } from '@/types/benefit.types'
+import { useTypedSelector } from '@/hooks/useTypedSelector'
 import { FC } from 'react'
 
-const benefits: Benefit[] = [
-	{
-		id: 1,
-		benefit: {
-			top: 'мы',
-			middle: '1',
-			bottom: 'на рынке'
-		}
-	},
-	{
-		id: 2,
-		benefit: {
-			top: 'гарантируем',
-			middle: '50%',
-			bottom: 'безопасность'
-		}
-	},
-	{
-		id: 3,
-		benefit: {
-			top: 'календарик за',
-			middle: '2001',
-			bottom: 'в подарок'
-		}
-	},
-	{
-		id: 4,
-		benefit: {
-			top: 'путешествие',
-			middle: '597',
-			bottom: 'дней'
-		}
-	}
-]
-
 const Benefits: FC = () => {
+	const { benefits } = useTypedSelector(state => state.benefits)
+
 	return (
 		<div className='grid grid-cols-2 gap-4'>
 			{benefits.slice(0, 4).map(({ benefit: { middle, top, bottom }, id }) => (
