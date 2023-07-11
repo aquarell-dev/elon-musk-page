671a5af34017243c6cda16004b0304f66719b467
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-11 20:02:51 +0300

    modified back-end link

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const defaultAxios = axios.create({
-	baseURL: 'http://127.0.0.1:8000/api/v4/',
+	baseURL: 'https://elon-musk-back-end.onrender.com/api/v4/',
 	headers: {
 		'Content-Type': 'application/json'
 	}
